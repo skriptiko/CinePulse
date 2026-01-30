e5024d7b363085fea700957d8ebf1e89ac55da51
anton.pleskach <anton.pleskach@mediamonks.com>
2026-01-30 12:09:03 +0200

    Clear project

--- apps/remote/.next/server/server-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__RSC_SERVER_MANIFEST =
-	'{"node":{},"edge":{},"encryptionKey":"3TOyofTeo1JcYZ2hs2xB6TeOquEwwM2NdLXzkUnQrdU="}';
--- packages/ui/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export const Button = React.forwardRef<
-	HTMLButtonElement,
-	React.ButtonHTMLAttributes<HTMLButtonElement>
->(({ className, ...props }, ref) => (
-	<button ref={ref} className={className} {...props} />
-));
-Button.displayName = "Button";
